a15ae3d249a95f0b38771e1a404244275934b7c8
Jakub Spólnik <85324038+lawos98@users.noreply.github.com>
2023-03-15 12:58:02 +0100

    init framework for backend and dataBase

---
+++ server/src/main/kotlin/pl/edu/agh/ii/io/jungleGirls/JungleGirlsApplication.kt
@@ -0,0 +1,11 @@
+package pl.edu.agh.ii.io.jungleGirls
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JungleGirlsApplication
+
+fun main(args: Array<String>) {
+	runApplication<JungleGirlsApplication>(*args)
+}
---
+++ server/src/test/kotlin/pl/edu/agh/ii/io/jungleGirls/JungleGirlsApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.edu.agh.ii.io.jungleGirls
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JungleGirlsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

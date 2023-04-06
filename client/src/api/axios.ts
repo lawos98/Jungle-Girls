3f775937f8e0d8cc395fe0105219703fe97b18be
Seszele <Seszele11@onet.pl>
2023-04-06 21:17:45 +0200

    fixed previous request issue

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,18 +1,27 @@
 import axios from 'axios';
-import Cookies from "js-cookie";
-
+import Cookies from 'js-cookie';
 
 const api = axios.create({
     baseURL: 'http://localhost:8080/api',
-    headers: {
-        Authorization: `Bearer ${Cookies.get('token')}`,
-    },
+});
+
+api.interceptors.request.use(config => {
+    const token = Cookies.get('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 api.interceptors.response.use(
     response => response,
     error => {
         console.log(error);
+        if (error.response && error.response.status === 401) {
+            // handle unauthorized error
+            // for example, redirect to login page or show error message
+            console.log("error.response.data.message: " + error.response.data.message);
+        }
         return Promise.reject(error);
     }
 );
--- client/src/components/login/LoginActions.ts
+++ client/src/components/login/LoginActions.ts
@@ -2,7 +2,14 @@ import api from "../../api/axios";
 import Cookies from "js-cookie";
 
 export const login = (email: string, password: string) => {
-    api.post('/login', {email, password}).then(response => {
+    let payload = {
+        username: email, // tymczasowy fix, bo backend wymaga username, a nie email
+        password: password
+    }
+    api.post('/login', payload).then(response => {
         Cookies.set('token', response.data.token, {httpOnly: true})
-    })
+        console.log("response.data.token: " + response.data.token)
+    }).catch(error => {
+        console.error(error);
+    });
 }

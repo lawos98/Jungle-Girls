efe1e2fd6dad6465ee32a7803ed6543063f18295
lawos <jakub.jan.spolnik@gmail.com>
2023-04-19 12:28:01 +0200

    improve score operations efficient 2 times

--- server/src/main/kotlin/pl/edu/agh/ii/io/jungleGirls/service/ScoreService.kt
+++ server/src/main/kotlin/pl/edu/agh/ii/io/jungleGirls/service/ScoreService.kt
@@ -21,22 +21,23 @@ class ScoreService(
     private val studentDescriptionService: StudentDescriptionService
 ) {
 
-    fun getScores(groupId:Long, lecturerId:Long):Either<String,List<ActivityScoreList>>{
-        if(!courseGroupService.existsByCourseId(groupId)) return "That course group is not accessible".left()
-        if(!rolePermissionService.checkUserPermission(lecturerId,Permissions.GRADE_VIEW) && !courseGroupService.checkLecturerGroup(lecturerId,groupId)){
+    fun getScores(groupId: Long, lecturerId: Long): Either<String, List<ActivityScoreList>> {
+        if (!courseGroupService.existsByCourseId(groupId)) return "That course group is not accessible".left()
+        if (!rolePermissionService.checkUserPermission(lecturerId, Permissions.GRADE_VIEW) && !courseGroupService.checkLecturerGroup(lecturerId, groupId)) {
             return "You don't have permission to view scores".left()
         }
         val scores = scoreRepository.getScoresByGroupId(groupId).collectList().block() ?: return "Server cannot find scores for groupId $groupId".left()
-        val studentList = courseGroupService.getAllStudentsByGroupId(groupId).map { StudentScore(it.id ?: return "Missing Id for user".left(),it.username,it.firstname,it.lastname,null) }
+        val studentList = courseGroupService.getAllStudentsByGroupId(groupId).map { StudentScore(it.id ?: return "Missing Id for user".left(), it.username, it.firstname, it.lastname, null) }
+        val studentMap = studentList.associateBy { it.id }
         return activityService.getAllActivityByGroupId(groupId).distinct().map { activity ->
-            val currentScoreList = scores.filter {it.activityId==activity.id }
-            val studentHavingScoreList = currentScoreList.map {elem->
-                val student = studentList.find{it.id==elem.studentId} ?: return "Server cannot find user ${elem.studentId} on table Score".left()
-                student.value=elem.value
+            val currentScoreList = scores.filter { it.activityId == activity.id }
+            val studentHavingScoreList = currentScoreList.mapNotNull { elem ->
+                val student = studentMap[elem.studentId] ?: return "Server cannot find user ${elem.studentId} on table Score".left()
+                student.value = elem.value
                 student
             }
             val studentNotHavingScoreList = studentList.filterNot { student -> studentHavingScoreList.map { it.id }.contains(student.id) }
-            ActivityScoreList(activity, (studentHavingScoreList+studentNotHavingScoreList).sortedBy { it.id })
+            ActivityScoreList(activity, (studentHavingScoreList + studentNotHavingScoreList).sortedBy { it.id })
         }.toCollection(ArrayList()).right()
     }
     fun getScore(user: LoginUser): Either<String, List<ActivityScore>> {
@@ -52,12 +53,16 @@ class ScoreService(
     fun updateScores(groupId: Long, lecturerId: Long, scoreList: List<ActivityScoreList>): Either<String, List<ActivityScoreList>> {
         if (!courseGroupService.existsByCourseId(groupId)) return "That course group is not accessible".left()
         if (!rolePermissionService.checkUserPermission(lecturerId, Permissions.GRADE_EDIT) && !courseGroupService.checkLecturerGroup(lecturerId, groupId)) return "You don't have permission to view scores".left()
+
+        val activities = activityService.getAllActivityByGroupId(groupId).map { it.id }.toSet()
+        val students = courseGroupService.getAllStudentsByGroupId(groupId).map { it.id }.toSet()
+
         scoreList.forEach { elem ->
             val activity = elem.activity
             if (activity != activityService.getById(activity.id!!)) return "Activity with id ${activity.id} has incorrect values".left()
-            if (!activityService.getAllActivityByGroupId(groupId).map { it.id }.contains(activity.id)) return "Activity with that id dont belong to this course group".left()
+            if (!activities.contains(activity.id)) return "Activity with that id dont belong to this course group".left()
             elem.students.forEach { student ->
-                if(!courseGroupService.getAllStudentsByGroupId(groupId).map { it.id }.contains(student.id)) return "Student with id ${student.id} dont belong to this course group with id $groupId".left()
+                if(!students.contains(student.id)) return "Student with id ${student.id} dont belong to this course group with id $groupId".left()
                 val newValue = student.value
                 val oldScore = scoreRepository.getScore(activity.id!!,student.id,groupId).block()
                 val oldValue = oldScore?.value
@@ -71,6 +76,6 @@ class ScoreService(
                 }}
             }
         return scoreList.right()
-        }
+    }
 
 }

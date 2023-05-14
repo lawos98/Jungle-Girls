d249b3821bcde8902f1812d95efa5c7b64892ce2
pawelgacek-admin <pawelgacek.pl@gmail.com>
2023-05-14 16:38:18 +0200

    added permission

--- migrations/v04_add_role_permission.sql
+++ migrations/v04_add_role_permission.sql
@@ -6,7 +6,9 @@ insert into permission(name) values ('Activity category manager');
 insert into permission(name) values ('Notification view');
 insert into permission(name) values ('CSV generation');
 insert into permission(name) values ('Temporary event view');
+insert into permission(name) values ('Notification creation');
 insert into permission(name) values ('Leaderboard view');
+
 
 insert into role_permission(role_id, permission_id,should_be_displayed) values (1,1,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (1,2,false);
@@ -17,6 +19,7 @@ insert into role_permission(role_id, permission_id,should_be_displayed) values (
 insert into role_permission(role_id, permission_id,should_be_displayed) values (1,7,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (1,8,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (1,9,false);
+insert into role_permission(role_id, permission_id,should_be_displayed) values (1,10,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (2,1,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (2,2,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (2,3,false);
@@ -26,6 +29,7 @@ insert into role_permission(role_id, permission_id,should_be_displayed) values (
 insert into role_permission(role_id, permission_id,should_be_displayed) values (2,7,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (2,8,true);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (2,9,true);
+insert into role_permission(role_id, permission_id,should_be_displayed) values (2,10,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (3,1,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (3,2,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (3,3,false);
@@ -34,7 +38,8 @@ insert into role_permission(role_id, permission_id,should_be_displayed) values (
 insert into role_permission(role_id, permission_id,should_be_displayed) values (3,6,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (3,7,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (3,8,false);
-insert into role_permission(role_id, permission_id,should_be_displayed) values (3,9,false);
+insert into role_permission(role_id, permission_id,should_be_displayed) values (3,9,true);
+insert into role_permission(role_id, permission_id,should_be_displayed) values (3,10,false);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (4,1,true);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (4,2,true);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (4,3,true);
@@ -43,4 +48,5 @@ insert into role_permission(role_id, permission_id,should_be_displayed) values (
 insert into role_permission(role_id, permission_id,should_be_displayed) values (4,6,true);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (4,7,true);
 insert into role_permission(role_id, permission_id,should_be_displayed) values (4,8,true);
-insert into role_permission(role_id, permission_id,should_be_displayed) values (4,9,false);
+insert into role_permission(role_id, permission_id,should_be_displayed) values (4,9,true);
+insert into role_permission(role_id, permission_id,should_be_displayed) values (4,10,false);

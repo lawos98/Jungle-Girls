5b9ec0302240b9b7fb87192cd67aeb9a3254f733
lawos <jakub.jan.spolnik@gmail.com>
2023-05-15 02:40:51 +0200

    remove secret Codes from roles and add functionality to secret Codes from a Course group

--- server/src/main/kotlin/pl/edu/agh/ii/io/jungleGirls/dto/RoleDto.kt
+++ server/src/main/kotlin/pl/edu/agh/ii/io/jungleGirls/dto/RoleDto.kt
@@ -1,6 +1,3 @@
 package pl.edu.agh.ii.io.jungleGirls.dto
-
-data class SecretCode(val code:String)
 data class UserUpdateRoleRequest(val roleId:Long,val userId:Long)
 data class RoleResponse(val roleId: Long,val name: String,val description:String)
-data class RoleUpdateResponse(val id:Long,val username:String,val firstname:String,val lastname:String,val roleId:Long)
